8511b7bcc664b898f55b2778a42d0fba67e62f10
Queen-Uwitonze <uwitonzeq@gmail.com>
2019-01-18 07:40:48 -0500

    first commit created html.file

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+// you need a variable for the user 
+// you need a variable for the initial amount first set to Zero
+// you will need a variable for the first deposit
+
+// create a function to collect the first depo, and once the user clicks on the "create account button" 
+// the first depo should be added to the initial amount which will also be added to the current balance
+// Create a variable for current balance
+
+// for all these functions remember to add (currentbalance).text("frw") 
+// you will create one function (Movement) with an if statment:if the user fills the deposit box  the input will be added to current balance
+// else input substracted
+
+// create 2 functions  with their buttons
+// .
+
+function  Account(name,balance) {
+    var name=this.name;
+}
+Account.prototype.deposit =function(){
+     console.log ( this.name + "you have created an account")
+}
+ 
+  
